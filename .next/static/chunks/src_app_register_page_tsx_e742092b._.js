cea0fc1d24d498a27704a39312d56060efbffe9e
ganes999 <ganesramadhan@gmail.com>
2025-05-29 10:49:33 +0700

    Add .next/static/chunks/src_app_register_page_tsx_e742092b._.js 1748490573737

---
+++ .next/static/chunks/src_app_register_page_tsx_e742092b._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_7ff578e4._.js",
+  "static/chunks/src_5155257c._.js"
+],
+    source: "dynamic"
+});
